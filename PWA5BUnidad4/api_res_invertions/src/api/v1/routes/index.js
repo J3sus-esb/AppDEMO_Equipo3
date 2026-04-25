bedc2a192fc919eb4a8b37db5549b9e1bcaec14b
J3sus-esb <jealpartidavi@ittepic.edu.mx>
2026-04-25 14:42:17 -0700

    Corrige rutas y exports en index.js de la API

--- PWA5BUnidad4/api_res_invertions/src/api/v1/routes/index.js
+++ PWA5BUnidad4/api_res_invertions/src/api/v1/routes/index.js
@@ -1,19 +1,17 @@
-//MASU: Education
+
 import { Router } from 'express'; 
 import config from '../../../config/config'; 
-// MASU: Import Routes 
+// MASU: Rutas Importadas
 import institutosRoutes from './institutos.routes.js';
 import etiquetasRoutes from './etiquetas.routes.js';
-//MASU: import ordersRoutes from './orders.routes'; 
 const routerAPI = (app) => { 
   const router = Router(); 
-  const api = config.API_URL; 
-  app.use(api, router); 
-  // MASU: Routes 
+  app.use('/api/v1', router); 
+  
+  // MASU: Rutas
   router.use('/institutos', institutosRoutes);
   router.use('/etiquetas', etiquetasRoutes);
-  //router.use('/orders', ordersRoutes); 
-  // Return Router 
+
   return router; 
 }; 
-module.exports = routerAPI;
+export default routerAPI;
